3dd12d9ca5fd559b88d4414050313691f9e89c49
Bilal <pro.boudjemline@gmail.com>
2026-05-04 14:50:15 +0200

    up

---
+++ tools/Vector2D.mjs
@@ -0,0 +1,37 @@
+export default class Vector2D
+{
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
+    constructor(x, y)
+    {
+        this.x_val = x;
+        this.y_val = y;
+    }
+
+    get x()
+    {
+        return this.x_val;
+    }
+
+    get y()
+    {
+        return this.y_val;
+    }
+
+    /**
+     * 
+     * @param {Vector2D} other 
+     */
+    add(other)
+    {
+        return new Vector2D((this.x_val + other.x), (this.y_val + other.y))
+    }
+
+    scale(k)
+    {
+        return new Vector2D(this.x_val*k, this.y_val*k)
+    }
+}
